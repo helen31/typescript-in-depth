df48869d88e14392150c5390a2397505af06166c
Olena_Hlukhovska <a30058422@gmail.com>
2022-04-27 10:41:19 +0200

    task 06.05. Dynamic Import Expression

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -2,3 +2,4 @@ export * from './reference-item';
 export * as UL from './university-librarian';
 export { default as RefBook } from './encyclopedia';
 export { default as Shelf } from './shelf';
+export * from './reader';
