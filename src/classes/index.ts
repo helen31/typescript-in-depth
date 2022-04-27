05a6cf671daeac9976f5b87a52646e3922467729
Olena_Hlukhovska <a30058422@gmail.com>
2022-04-27 11:13:28 +0200

    task 06.06. Type-Only Imports and Exports

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -3,3 +3,4 @@ export * as UL from './university-librarian';
 export { default as RefBook } from './encyclopedia';
 export { default as Shelf } from './shelf';
 export * from './reader';
+export type { Library } from './library';
